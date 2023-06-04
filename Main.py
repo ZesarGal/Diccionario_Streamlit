a375d364c3402415a3d3a4e0707c6292ae4ed27d
ZesarGal <114784502+ZesarGal@users.noreply.github.com>
2023-06-03 19:00:03 -0600

    Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import base64
 
-file1_ = open("./Im1.png", "rb")
+#file1_ = open("./Im1.png", "rb")
 
 
 #Portada
-contents1 = file1_.read()
-data_url1 = base64.b64encode(contents1).decode("utf-8")
-file1_.close()
+#contents1 = file1_.read()
+#data_url1 = base64.b64encode(contents1).decode("utf-8")
+#file1_.close()
 
-import streamlit as st
+#import streamlit as st
 
 st.markdown("![Alt Text](https://media.giphy.com/media/XxHVJxuogNP32/giphy.gif)")
 
@@ -18,10 +18,10 @@ st.title('''Diccionario\n
 
             ''',)
 
-contents = file.read()
-data_url = base64.b64encode(contents).decode('utf-8-sig')
-file.close()
-st.markdown(f'<img src="data:image/gif;base64,{data_url}>',unsafe_allow_html = True)
+#contents = file.read()
+#data_url = base64.b64encode(contents).decode('utf-8-sig')
+#file.close()
+#st.markdown(f'<img src="data:image/gif;base64,{data_url}>',unsafe_allow_html = True)
 
 
 
