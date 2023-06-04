d603c656d8df791bba8d399ae417752a0d22f145
ZesarGal <114784502+ZesarGal@users.noreply.github.com>
2023-06-03 18:24:57 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sympy as sy
+from matplotlib import pyplot as plt
+from sympy.plotting.plot import MatplotlibBackend, Plot
+from sympy.plotting import plot3d,plot3d_parametric_line
+import plotly as ply
+import base64
+
+file1_ = open("./Im1.png", "rb")
+
+
+#Portada
+contents1 = file1_.read()
+data_url1 = base64.b64encode(contents1).decode("utf-8")
+file1_.close()
+
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url1}" alt="Escudo_Unam">',
+    unsafe_allow_html=True,
+)
+
+
+st.title('''UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO\n
+
+            ''',)
+
+st.header('FACULTAD DE ESTUDIOS SUPERIORES ACATLÁN')
+
+st.header('MÉTODOS NUMERICOS II')
+
+st.subheader('''
+PROFESOR\n
+_Julio César Galindo López_
+''')
+
+st.subheader('''''')
+
+st.subheader('''
+        Presenta \n
+        *Nombre de los Integrantes
+''')
+
+
+
